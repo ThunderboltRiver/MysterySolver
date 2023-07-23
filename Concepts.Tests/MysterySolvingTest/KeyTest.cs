3ec39f8c915ecf1425de246cd559cb77ec033ef8
ThunderboltRiver <lostbleuseas@outlook.jp>
2023-07-23 20:22:34 +0900

    鍵の同一性に関するテスト作成

---
+++ Concepts.Tests/MysterySolvingTest/KeyTest.cs
@@ -0,0 +1,79 @@
+namespace Concepts.Tests.MysterySolvingTest
+{
+    public class KeyTests
+    {
+        [Theory]
+        [InlineData("mst-key-001")]
+        [InlineData("mst-key-002")]
+        public void Key_コンストラクタでキーIDとして正しいを指定するとキーが生成される(string keyId)
+        {
+            var key = new Key(keyId);
+            Assert.NotNull(key);
+        }
+
+        [Theory]
+        [InlineData("mst-key-ksaf")]
+        [InlineData("mst-key-001x")]
+        [InlineData("-mst-key-001")]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Key_コンストラクタで間違えたキーIDを指定するとArgumetExceptionをスローする(string keyId)
+        {
+            Assert.Throws<ArgumentException>(() => new Key(keyId));
+        }
+
+        [Fact]
+        public void Key_同一性_キーIDが同じなら同じキー()
+        {
+            var key1 = new Key("mst-key-001");
+            var key2 = new Key("mst-key-001");
+            Assert.Equal(key1, key2);
+        }
+        [Fact]
+        public void Key_同一性_キーIDが異なるなら異なるキー()
+        {
+            var key1 = new Key("mst-key-001");
+            var key2 = new Key("mst-key-002");
+            Assert.NotEqual(key1, key2);
+        }
+
+        [Fact]
+        public void Key_同一性_キーIDが同じならハッシュ値は同じ()
+        {
+            var key1 = new Key("mst-key-001");
+            var key2 = new Key("mst-key-001");
+            Assert.Equal(key1.GetHashCode(), key2.GetHashCode());
+        }
+        [Fact]
+        public void Key_同一性_キーIDが異なるならハッシュ値は異なる()
+        {
+            var key1 = new Key("mst-key-001");
+            var key2 = new Key("mst-key-002");
+            Assert.NotEqual(key1.GetHashCode(), key2.GetHashCode());
+        }
+
+        [Fact]
+        public void Key_同一性_キーIDが同じなら等価演算子はtrue()
+        {
+            var key1 = new Key("mst-key-001");
+            var key2 = new Key("mst-key-001");
+            Assert.True(key1 == key2);
+        }
+        [Fact]
+        public void Key_同一性_キーIDが異なるなら等価演算子はfalse()
+        {
+            var key1 = new Key("mst-key-001");
+            var key2 = new Key("mst-key-002");
+            Assert.False(key1 == key2);
+        }
+
+        [Fact]
+        public void Key_KeyId_オブジェクトによって再構築できる()
+        {
+            var keyId = new KeyId("mst-key-001");
+            var key = new Key(keyId);
+            var key2 = new Key("mst-key-001");
+            Assert.Equal(key, key2);
+        }
+    }
+}
