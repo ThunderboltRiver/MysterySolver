13b938081d69c480ce3aa1e0e88c6e9f4617cdcb
ThunderboltRiver <lostbleuseas@outlook.jp>
2023-08-12 18:53:44 +0900

    認証にまつわるテストを修正

--- Concepts.Tests/MysterySolvingTest/MysteryTest.cs
+++ Concepts.Tests/MysterySolvingTest/MysteryTest.cs
@@ -9,7 +9,9 @@ public class MysteryTest
     {
         var mystery = new Mystery("mst-mystery-001", "mst-key-001", "mst-mystery-000");
         var key = new Key("mst-key-001");
-        var solvedMystery = mystery.Authenticate(key, new SolvedMystery(new MysteryId("mst-mystery-000")));
+        var solvedRoute = new SolvedRoute();
+        solvedRoute.AddNew(new SolvedMystery(new MysteryId("mst-mystery-000")));
+        var solvedMystery = mystery.Authenticate(key, solvedRoute);
         Assert.NotNull(solvedMystery);
     }
 
@@ -18,7 +20,9 @@ public class MysteryTest
     {
         var mystery = new Mystery("mst-mystery-001", "mst-key-001", "mst-mystery-000");
         var key = new Key("mst-key-001");
-        var solvedMystery = mystery.Authenticate(key, new SolvedMystery(new MysteryId("mst-mystery-001")));
+        var solvedRoute = new SolvedRoute();
+        solvedRoute.AddNew(new SolvedMystery(new MysteryId("mst-mystery-001")));
+        var solvedMystery = mystery.Authenticate(key, solvedRoute);
         Assert.Null(solvedMystery);
     }
 
