0aa4a9d591fbbd17f33a4b43ba5e4c13a434e6ad
ThunderboltRiver <lostbleuseas@outlook.jp>
2023-08-13 11:23:18 +0900

    namespaceの書き方を修正

--- Concepts/MysterySolving/SolvedMystery.cs
+++ Concepts/MysterySolving/SolvedMystery.cs
@@ -1,24 +1,25 @@
-namespace Concepts.MysterySolving;
-
-/// <summary>
-/// 解かれた謎を表すオブジェクト
-/// </summary>
-public record SolvedMystery
+namespace Concepts.MysterySolving
 {
-    private readonly MysteryId _mysteryId; //解かれた謎のID
-    internal SolvedMystery(MysteryId mysteryId)
-    {
-        _mysteryId = mysteryId;
-    }
-
     /// <summary>
-    /// 特定の謎によって解かれたかどうかを判定する
+    /// 解かれた謎を表すオブジェクト
     /// </summary>
-    /// <param name="mysteryId">謎のid</param>
-    /// <returns>bool</returns>
-    internal bool IsAuthenticatedBy(MysteryId mysteryId)
+    public record SolvedMystery
     {
-        return _mysteryId == mysteryId;
-    }
+        private readonly MysteryId _mysteryId; //解かれた謎のID
+        internal SolvedMystery(MysteryId mysteryId)
+        {
+            _mysteryId = mysteryId;
+        }
 
+        /// <summary>
+        /// 特定の謎によって解かれたかどうかを判定する
+        /// </summary>
+        /// <param name="mysteryId">謎のid</param>
+        /// <returns>bool</returns>
+        internal bool IsAuthenticatedBy(MysteryId mysteryId)
+        {
+            return _mysteryId == mysteryId;
+        }
+
+    }
 }
